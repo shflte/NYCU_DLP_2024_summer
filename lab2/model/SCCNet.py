31d783bf39f0de3593ea36a5210845458bbba862
Jim Chen <shflte1314527@gmail.com>
2024-07-18 13:43:14 +0800

    square layer

--- lab2/model/SCCNet.py
+++ lab2/model/SCCNet.py
@@ -6,9 +6,10 @@ import torch.nn as nn
 # reference paper: https://ieeexplore.ieee.org/document/8716937
 class SquareLayer(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(SquareLayer, self).__init__()
+
     def forward(self, x):
-        pass
+        return x ** 2
 
 class SCCNet(nn.Module):
     def __init__(self, numClasses=0, timeSample=0, Nu=0, C=0, Nc=0, Nt=0, dropoutRate=0):
