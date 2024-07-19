ddfa7ac7b241f6f3115b1a824278b5a9336c5d2b
Jim Chen <shflte1314527@gmail.com>
2024-07-19 10:43:25 +0800

    add plt.figure() to avoid overlaying multiple lines on the same plot

--- lab2/utils.py
+++ lab2/utils.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 
 
-def show_accuracy(acc):
+def show_accuracy(acc, fileName):
+    plt.figure()
     epochs = range(1, len(acc) + 1)
 
     plt.plot(epochs, acc, 'b', label='Accuracy')
@@ -10,17 +11,17 @@ def show_accuracy(acc):
     plt.ylabel('Accuracy')
     plt.legend()
     plt.show()
-    plt.savefig('accuracy.png')
+    plt.savefig(f'{fileName}_accuracy.png')
 
 
-def show_learning_curve(loss):
+def show_learning_curve(loss, fileName):
+    plt.figure()
     epochs = range(1, len(loss) + 1)
 
-    plt.figure(figsize=(8, 5))
     plt.plot(epochs, loss, 'b', label='Loss')
     plt.title('Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
     plt.show()
-    plt.savefig('loss.png')
+    plt.savefig(f'{fileName}_loss.png')
