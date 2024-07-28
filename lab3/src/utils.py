49c1c5aab2b2c89064f56f51e31df444ec8c2946
Jim Chen <shflte1314527@gmail.com>
2024-07-28 16:10:28 +0800

    implemented dice_score()

--- lab3/src/utils.py
+++ lab3/src/utils.py
@@ -1,5 +1,7 @@
-def dice_score(pred_mask, gt_mask):
-    # implement the Dice score here
-    
-    assert False, "Not implemented yet!"
+import numpy as np
 
+def dice_score(pred_mask, gt_mask):
+    assert pred_mask.shape == gt_mask.shape
+    logical_and = np.logical_and(pred_mask, gt_mask)
+    logical_not_and = np.logical_and(np.logical_not(pred_mask), np.logical_not(gt_mask))
+    return 2 * (logical_and.sum() + logical_not_and.sum()) / (pred_mask.size + gt_mask.size)
