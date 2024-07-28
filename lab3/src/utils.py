65ae4f454cbbfba055600e4826ca03c127074c01
Jim Chen <shflte1314527@gmail.com>
2024-07-28 16:12:32 +0800

    update dice_score() for better readibility

--- lab3/src/utils.py
+++ lab3/src/utils.py
@@ -2,6 +2,5 @@ import numpy as np
 
 def dice_score(pred_mask, gt_mask):
     assert pred_mask.shape == gt_mask.shape
-    logical_and = np.logical_and(pred_mask, gt_mask)
-    logical_not_and = np.logical_and(np.logical_not(pred_mask), np.logical_not(gt_mask))
-    return 2 * (logical_and.sum() + logical_not_and.sum()) / (pred_mask.size + gt_mask.size)
+    logical_xnor = np.logical_not(np.logical_xor(pred_mask, gt_mask))
+    return 2 * (logical_xnor.sum()) / (pred_mask.size + gt_mask.size)
