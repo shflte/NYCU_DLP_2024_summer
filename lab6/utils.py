1e1620efbdf0a43c77376b96eebbb6f752244ca9
Jim Chen <shflte1314527@gmail.com>
2024-08-23 01:20:38 +0800

    add show_accuracies()

--- lab6/utils.py
+++ lab6/utils.py
@@ -22,3 +22,13 @@ def show_losses(losses):
     plt.ylabel("Loss")
     plt.legend()
     plt.savefig(f"loss.png")
+
+
+def show_accuracies(accuracies):
+    epochs = [5 * (i + 1) for i in range(len(accuracies))]
+    plt.figure(figsize=(10, 5))
+    plt.plot(epochs, accuracies, label="Validation Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    plt.savefig("accuracy.png")
